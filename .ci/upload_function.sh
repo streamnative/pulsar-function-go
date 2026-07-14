6f955d9bc24bfa8b8e381f4227212b9d9ece0938
jiangpengcheng <scjiangpengcheng@gmail.com>
2026-07-15 01:16:37 +0800

    Keep default e2e on released runtime image

--- .ci/upload_function.sh
+++ .ci/upload_function.sh
@@ -28,4 +28,3 @@ CLUSTER=sn-platform
 kubectl cp "${PULSAR_HOME}/.ci/examples" "${NAMESPACE}/${CLUSTER}-pulsar-broker-0:/pulsar/"
 
 kubectl exec -n ${NAMESPACE} ${CLUSTER}-pulsar-broker-0 -- bin/pulsar-admin packages upload function://public/default/exec-exclamation --path /pulsar/examples/exclamationGo/excla --description "test executable function"
-kubectl exec -n ${NAMESPACE} ${CLUSTER}-pulsar-broker-0 -- bin/pulsar-admin packages upload function://public/default/exec-context-publish-avro --path /pulsar/examples/contextPublishAvroGo/contextpublishavro --description "test executable context publish with avro schema"
